3a592c39477732c959c41e3055a445762434bba3
Json_Dev <jasonlopes132@gmail.com>
2024-07-10 21:06:11 -0300

    Update ex_for_array0.php

--- ex_for_array0.php
+++ ex_for_array0.php
@@ -1,52 +1,76 @@
 <?php
-    
-    if(isset($_POST["btn_iniciar"])){
-    $numero = array();
-         
-        $numero[]= $_POST["n1"];
-        $numero[]= $_POST["n2"];
-        $numero[]= $_POST["n3"];
-        $numero[]= $_POST["n4"];
-        $numero[]= $_POST["n5"];
-        $numero[]= $_POST["n6"];
-        $numero[]= $_POST["n7"];
+$qtd_elementos = 7;
 
-        echo "TOTAL DE DIGITOS: " . count($numero) . "<br>";
+if (isset($_POST['btn_exibir'])) {
 
-     for ($i=0; $i < count($numero); $i++){
+    $n1 = trim($_POST['n1']);
+    $n2 = trim($_POST['n2']);
+    $n3 = trim($_POST['n3']);
+    $n4 = trim($_POST['n4']);
+    $n5 = trim($_POST['n5']);
+    $n6 = trim($_POST['n6']);
+    $n7 = trim($_POST['n7']);
 
-                echo "O numero Digitado foi: " . $numero[$i] . "<br>";
+    if (
+        $n1 == '' || $n2 == '' || $n3 == '' || $n4 == '' || $n5 == '' || $n5 == ''
+        || $n6 == '' ||  $n7 == ''
+    )
+        echo 'Preencher TODOS OS NUMEROS';
+    else {
+        //Cria a variável array
+        $numeros = array();
+        //Guarda os valores
+        $numeros[] = $n1;
+        $numeros[] = $n2;
+        $numeros[] = $n3;
+        $numeros[] = $n4;
+        $numeros[] = $n5;
+        $numeros[] = $n6;
+        $numeros[] = $n7;
+
+        for ($i = 0; $i < count($numeros); $i++) {
+            echo 'Número: ' . $numeros[$i] . '. Sua posição é: [' . $i . ']<br>';
+        }
+
+        echo '----------------------------------------------------------<br>';
+        echo '----- NUMEROS PARES ENCONTRADOS -----<br>';
+        echo '-----------------------------------------------------------<br>';
+        $flag = false;
+        //LAÇO DE REPETIÇÃO ENCONTRANDO OS NUMEROS PARES
+        for($i=0; $i < count($numeros); $i++)
+        {
+            if($numeros[$i] % 2 == 0){
+                // Verifica se a nao for par usando o $flag
+                $flag = true;
+                echo 'Número: ' . $numeros[$i] . '. Sua posição é: [' . $i . ']<br>';
+            }
+        }
+            // caso contrario da sua equação, ele identifica e exibe o ECHO que voce decidir, texto ou variavel
+            if(!$flag)
+            {
+                echo "Não foi encontrado um numero Par";
+            }
     }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>
-        Digite 07 Numeros !
-    </h1>
     <form action="ex_for_array0.php" method="post">
-        <label>Digite um Numero: </label>
-        <input type="number" name="n1" value="<?isset($n1)?>"><br>
-        <label>Digite um Numero: </label>
-        <input type="number" name="n2" value="<?isset($n2)?>"><br>
-        <label>Digite um Numero: </label>
-        <input type="number" name="n3" value="<?isset($n3)?>"><br>
-        <label>Digite um Numero: </label>
-        <input type="number" name="n4" value="<?isset($n4)?>"><br>
-        <label>Digite um Numero: </label>
-        <input type="number" name="n5" value="<?isset($n5)?>"><br>
-        <label>Digite um Numero: </label>
-        <input type="number" name="n6" value="<?isset($n6)?>"><br>
-        <label>Digite um Numero: </label>
-        <input type="number" name="n7" value="<?isset($n7)?>"><br>
+        <?php for ($i = 1; $i <= $qtd_elementos; $i++) { ?>
+        <label>Numero <?= $i ?></label>
+        <input name="n<?= $i ?>">
         <br>
-        <button name="btn_iniciar"> - Iniciar Contagem - </button>
+        <?php } ?>
+        <button name="btn_exibir">Exibir</button>
     </form>
 </body>
+
 </html>
