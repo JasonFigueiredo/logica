70651ea75651a054ce382a56c51bb92b0c06ac10
Json_Dev <jasonlopes132@gmail.com>
2024-07-31 14:54:36 -0300

    Atualização de dados

--- ex3_formulario.php
+++ ex3_formulario.php
@@ -9,13 +9,13 @@ if(isset($_POST["btn_enviar"]))
     $valor = $_POST["valor"];
     $resumo = $_POST["resumo"];
 
-    echo "Nome do Livro: " . $nome .
-    "Ano: " . $ano .
-    "<br>Editora: " . $editora .
-    "Qtd de Paginas: " . $qtd .
-    "<br>Autores: " . $autores .
-    "<br>Valor: " . $valor .
-    "<br>Resumo: " . $resumo;
+    echo "Nome do Livro: " . $nome . "<br>" .
+    "Ano: " . $ano . "<br>" .
+    "Editora: " . $editora . "<br>" .
+    "Qtd de Paginas: " . $qtd . "<br>" .
+    "Autores: " . $autores . "<br>" .
+    "Valor: " . $valor . "<br>" .
+    "Resumo: " . $resumo;
 }
 ?>
 <!DOCTYPE html>
@@ -30,26 +30,19 @@ if(isset($_POST["btn_enviar"]))
 <body>
     <form method="post" action="ex3_formulario.php">
         <label>Nome do Livro:</label>
-        <input name="nome" placeholder="Digite...">
-
+        <input name="nome" placeholder="Digite..."><br>
         <label>Ano:</label>
-        <input name="ano" placeholder="Digite...">
-
+        <input name="ano" placeholder="Digite..."><br>
         <label>Editora:</label>
-        <input name="editora" placeholder="Digite...">
-
+        <input name="editora" placeholder="Digite..."><br>
         <label>Qtd de páginas:</label>
-        <input name="qtd" placeholder="Digite...">
-
+        <input name="qtd" placeholder="Digite..."><br>
         <label>Autores:</label>
-        <input name="autores" placeholder="Digite...">
-
+        <input name="autores" placeholder="Digite..."><br>
         <label>Valor:</label>
-        <input name="valor" placeholder="Digite...">
-
+        <input name="valor" placeholder="Digite..."><br>
         <label>Resumo:</label>
-        <input name="resumo" placeholder="Digite...">
-
+        <input name="resumo" placeholder="Digite..."><br>
         <button name="btn_enviar">Enviar</button>
     </form>
 </body>
