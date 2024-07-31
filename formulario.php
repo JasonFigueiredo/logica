76d05e44e8ae7cff95d8bbe20caf13135defc3f8
Json_Dev <jasonlopes132@gmail.com>
2024-07-31 13:15:17 -0300

    Complementos de dados FORM
    Foi feita a atualização e complementação de dados de um formulário de dados pessoais

--- formulario.php
+++ formulario.php
@@ -1,24 +1,37 @@
 <?php
-if(isset($_POST["btn_enviar"])) {
-    $nome = $_POST["nome_dog"];
-    $idade = $_POST["idade"];
+    if(isset($_POST['btn_enviar'])){
 
-    echo "O Nome do dog é: " . $nome . "<br>Idade do Dog: " . $idade;
-}
+        $nome = $_POST["nomecompleto"];
+        $rua = $_POST["rua"];
+        $bairro = $_POST["bairro"];
+        $cep = $_POST["cep"];
+        $num = $_POST["numero"];
+        
+        echo "Meu nome é: <br>" . $nome . "<br>" . "Moro na rua:<br>" . $rua . "<br>" . 
+        "No Bairro:<br>" . $bairro . "<br>" . "No Cep:<br>" . $cep . "<br>" . "No numero:<br>" . $num;
+    }
 ?>
+
+
 <!DOCTYPE html>
-<html lang="PT-BR">
+<html lang="pt-br"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dados Pesoais</title>
 </head>
 <body>
-    <form method="post"  action="formulario.php">
-        <label>Digite o nome do Seu Cachoro</label>
-        <input name="nome_dog" placeholder="Digite aqui...">
-        <label>Digite a idade do seu Cachorro</label>
-        <input name="idade" placeholder="Digite aqui...">
+    <form action="formulario.php" method="post">
+        <label>Nome Completo: </label>
+        <input type="text" name="nomecompleto"><br>
+        <label>Rua: </label>
+        <input type="text" name="rua"><br>
+        <label>Bairro: </label>
+        <input type="text" name="bairro"><br>
+        <label>CEP: </label>
+        <input type="texnumbert" name="cep"><br>
+        <label>Numero: </label>
+        <input type="number" name="numero"><br>
         <button name="btn_enviar">Enviar</button>
     </form>
 </body>
