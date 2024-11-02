1ea3b3b549e1fcf66abe1de865a4132fdc767397
Jason Lopes <jasonlopes132@gmail.com>
2024-11-02 11:42:36 -0300

    ATT IMC

--- Classe/IMC.php
+++ Classe/IMC.php
@@ -1,25 +1,24 @@
 <?php
 
-    class IMC{
-        
-        public function CalcularIMC($peso, $altura){
-         $imc = $peso / ($altura * $altura)
-         return $imc;
-        } 
-        public function ClassificaçãoPeso($imc){
-            $classificação = "";
+class Imc
+{
 
-            if ($imc >= 0 && $imc <= 18,4){
-                $classificação = "ABAIXO DO PESO";
-            }
-            else if ($imc >= 18,5 && $imc <= 24,9)
-            {
-                $classificacao = 'PESO NORMAL';
-            } 
-            else if ($imc >= 25 && $imc <= 40) 
-            {
-                $classificacao = 'ACIMA DO PESO';
-            }
-            return $classificacao;
-        }
+    public function calcularImc($peso, $altura)
+    {
+        $imc = $peso / ($altura * $altura);
+        return $imc;
     }
+    public function classificacaoPeso($imc)
+    {
+        $classificação = "";
+
+        if ($imc >= 0 && $imc <= 18.4) {
+            $classificação = 'Abaixo do Peso';
+        } else if ($imc >= 18.5 && $imc <= 24.9) {
+            $classificação = 'PESO NORMAL';
+        } else if ($imc >= 25 && $imc <= 40) {
+            $classificação = 'Acima do Peso';
+        }
+        return $classificação;
+    }
+}
